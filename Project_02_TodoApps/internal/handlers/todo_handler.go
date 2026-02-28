1c20e5389828757abdee839f43ef4d05ea00fcf1
HoiCham <p.n.thang0101@gmail.com>
2026-03-01 03:53:53 +0700

    update todo handler

--- Project_02_TodoApps/internal/handlers/todo_handler.go
+++ Project_02_TodoApps/internal/handlers/todo_handler.go
@@ -15,6 +15,11 @@ type CreateTodoInput struct {
 	Completed bool   `json:"completed"`
 }
 
+type UpdateTodoInput struct {
+	Title     *string `json:"title"`
+	Completed *bool   `json:"completed"`
+}
+
 func CreateTodoHandler(pool *pgxpool.Pool) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var input CreateTodoInput
@@ -62,5 +67,50 @@ func GetTodoHandler(pool *pgxpool.Pool) gin.HandlerFunc {
 		}
 
 		c.JSON(http.StatusOK, todos)
+	}
+}
+
+func UpdateTodoHandler(pool *pgxpool.Pool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+
+		var input UpdateTodoInput
+		if err := c.ShouldBindJSON(&input); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if input.Title == nil && input.Completed == nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "At least on field(title or completed) must be provided"})
+			return
+		}
+
+		existing, err := repository.GetTodoById(pool, id)
+		if err != nil {
+			if errors.Is(err, pgx.ErrNoRows) {
+				c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+				return
+			}
+
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		title := existing.Title
+		if input.Title != nil {
+			title = *input.Title
+		}
+
+		completed := existing.Completed
+		if input.Completed != nil {
+			completed = *input.Completed
+		}
+
+		result, err := repository.UpdateTodo(pool, id, title, completed)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, result)
 	}
 }
--- Project_02_TodoApps/internal/repository/todo_repository.go
+++ Project_02_TodoApps/internal/repository/todo_repository.go
@@ -93,3 +93,25 @@ func GetTodoById(pool *pgxpool.Pool, id string) (*models.Todo, error) {
 
 	return &todo, nil
 }
+
+func UpdateTodo(pool *pgxpool.Pool, id string, title string, completed bool) (*models.Todo, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var query = "UPDATE todos SET title = $1, completed = $2, update_at = CURRENT_TIMESTAMP WHERE id = $3 returning id, title, completed, created_at,  updated_at"
+
+	var todo models.Todo
+	err := pool.QueryRow(ctx, query, title, completed, id).Scan(
+		&todo.ID,
+		&todo.Title,
+		&todo.Completed,
+		&todo.CreatedAt,
+		&todo.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &todo, nil
+}
