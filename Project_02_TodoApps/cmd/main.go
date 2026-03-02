32ae6e39a079590c0f59e0e36ab3990d51a09224
HoiCham <p.n.thang0101@gmail.com>
2026-03-03 00:15:31 +0700

    group and protect api

--- Project_02_TodoApps/cmd/main.go
+++ Project_02_TodoApps/cmd/main.go
@@ -40,14 +40,19 @@ func main() {
 		})
 	})
 
-	router.GET("/todos", handlers.GetAllTodosHandler(pool))
-	router.POST("/todos", handlers.CreateTodoHandler(pool))
-	router.GET("/todos/:id", handlers.GetTodoHandler(pool))
-	router.PUT("/todos/:id", handlers.UpdateTodoHandler(pool))
-	router.DELETE("/todos/:id", handlers.DeleteTodoHandler(pool))
-
 	router.POST("/auth/register", handlers.CreateUserHandler(pool))
 	router.POST("/auth/login", handlers.LoginHandler(pool, cfg))
+
+	protected := router.Group("/todos")
+	protected.Use(middleware.AuthMiddleware(cfg))
+
+	{
+		protected.GET("", handlers.GetAllTodosHandler(pool))
+		protected.POST("", handlers.CreateTodoHandler(pool))
+		protected.GET("/:id", handlers.GetTodoHandler(pool))
+		protected.PUT("/:id", handlers.UpdateTodoHandler(pool))
+		protected.DELETE("/:id", handlers.DeleteTodoHandler(pool))
+	}
 
 	router.GET("/protected-test", middleware.AuthMiddleware(cfg), handlers.TestProtectedHandler())
 
